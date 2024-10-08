88584623c1960127ce588a6387ea79c29283c110
Nataliia Kovalova <natusya.zira@outlook.com>
2024-10-08 23:14:47 +0300

    animate buttons

--- index.js
+++ index.js
@@ -3,44 +3,58 @@ for (let i = 0; i<numberOfButtons; i++){
     document.querySelectorAll('button')[i].addEventListener('click', function(){
     
     let buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case 'w':
-                let crash = new Audio('sounds/crash.mp3');
-                crash.play();
-                break;
-            case 'a':
-                let kick = new Audio('sounds/kick-bass.mp3');
-                kick.play();
-                break;
-            case 's':
-                let snare = new Audio('sounds/snare.mp3');
-                snare.play();
-                break;
-            case 'd':
-                let tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play();
-                break; 
-            case 'j':
-                let tom2 = new Audio('sounds/tom-2.mp3');
-                tom2.play();
-                break; 
-            case 'k':
-                let tom3 = new Audio('sounds/tom-3.mp3');
-                tom3.play();
-                break;  
-            case 'l':
-                let tom4 = new Audio('sounds/tom-4.mp3');
-                tom4.play();
-                break;     
-            default:
-                break;
-        }
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
     };
 
+document.addEventListener('keydown', function(e){
+    makeSound(e.key)
+    buttonAnimation(e.key);
+});
 
+function makeSound(key){
+switch (key) {
+    case 'w':
+        let crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+    case 'a':
+        let kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+    case 's':
+        let snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+    case 'd':
+        let tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break; 
+    case 'j':
+        let tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break; 
+    case 'k':
+        let tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;  
+    case 'l':
+        let tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;     
+    default:
+        break;
+}
+}
 
+function buttonAnimation (currentKey){
 
+    let activeButton = document.querySelector('.' + currentKey);
+    activeButton.classList.add('pressed');
 
-    // let audio = new Audio('sounds/tom-1.mp3');
-    // audio.play();
+    setTimeout(() => {
+        activeButton.classList.remove('pressed');
+      }, "150");
+
+}
